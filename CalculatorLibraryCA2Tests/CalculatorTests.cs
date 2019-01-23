abaa401a61f9da924d949957caa4721694ecb9f2
barrykeegan <barry.keegan@gmail.com>
2019-01-23 22:08:40 +0000

    Added subtract method tests before adding subtract method

--- CalculatorLibraryCA2Tests/CalculatorTests.cs
+++ CalculatorLibraryCA2Tests/CalculatorTests.cs
@@ -21,5 +21,16 @@ namespace CalculatorLibraryCA2.Tests
             Assert.AreEqual(4, Calculator.Add(2, 2));
             Assert.AreEqual(5, Calculator.Add(2, 3));
         }
+
+        [TestMethod()]
+        public void SubtractTest()
+        {
+            Assert.AreEqual(0, Calculator.Subtract(0, 0));
+            Assert.AreEqual(1, Calculator.Subtract(1, 0));
+            Assert.AreEqual(0.5, Calculator.Subtract(1, 0.5));
+            Assert.AreEqual(1, Calculator.Subtract(0, -1));
+            Assert.AreEqual(0, Calculator.Subtract(2, 2));
+            Assert.AreEqual(-1, Calculator.Subtract(2, 3));
+        }
     }
 }
