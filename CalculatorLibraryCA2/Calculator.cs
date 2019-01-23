4d7b6a4891c55c6b4e220b74771aef743f65d071
barrykeegan <barry.keegan@gmail.com>
2019-01-23 22:09:45 +0000

    Added stub subtract method to return 0, to pass first assert

--- CalculatorLibraryCA2/Calculator.cs
+++ CalculatorLibraryCA2/Calculator.cs
@@ -13,5 +13,10 @@ namespace CalculatorLibraryCA2
             //return 0.0; Original
             return n1 + n2; //Version 2
         }
+
+        public static double Subtract(double n1, double n2)
+        {
+            return 0.0; //Original
+        }
     }
 }
